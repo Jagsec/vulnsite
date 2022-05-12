ecc57b3bc61b2aa1b90c677ed68a689be345b8bd
Jagsec <jaimegovea@protonmail.com>
2022-05-12 12:26:50 -0500

    changes to the profile template, __init__ file and run file

--- interface/__init__.py
+++ interface/__init__.py
@@ -3,7 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-from .secrets import username, password, host, database, secret_key
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+username = getenv('USERNAME')
+password = getenv('PASSWORD')
+host = getenv('HOST')
+database = getenv('DATABASE')
+secret_key = getenv('SECRET')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@{host}/{database}'
@@ -21,8 +30,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'loginPage'
 login_manager.login_message_category = 'info'
 
-from interface.routes.globalRoutes import homePage, notFoundPage, notAuthenticatedPage
 from interface.routes.userRoutes import loginPage, logoutPage, registerPage, profilePage, editProfilePage
 from interface.routes.challengeRoutes import challengePage
+from interface.routes.globalRoutes import homePage, notFoundPage, notAuthenticatedPage
 app.register_error_handler(404, notFoundPage)
 app.register_error_handler(401, notAuthenticatedPage)
